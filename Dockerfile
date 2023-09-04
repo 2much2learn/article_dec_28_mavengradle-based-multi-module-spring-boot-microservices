30d4d50ff873e382aa49c1cb5d2a4952ba2e32d2
Carlos L. krlosaren@gmail.com <krlosaren@gmail.com>
2023-09-04 06:54:03 -0300

    [ADD] add minikube start

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,7 @@ COPY service-b/build/libs/service-b-0.0.1-SNAPSHOT.jar /service-b-0.0.1-SNAPSHOT
 COPY service-c/build/libs/service-c-0.0.1-SNAPSHOT.jar /service-c-0.0.1-SNAPSHOT.jar
 
 EXPOSE 8080
+
+CMD [ "java" ]
+
+ENTRYPOINT [ "-jar", "" ]
--- service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
+++ service-a/src/main/java/com/toomuch2learn/microservices/servicea/client/ServiceBClient.java
@@ -1,9 +1,10 @@
 package com.toomuch2learn.microservices.servicea.client;
 
-import com.toomuch2learn.microservices.servicea.model.Greeting;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.toomuch2learn.microservices.servicea.model.Greeting;
 
 @FeignClient(value = "serviceb", url = "${serviceb.url}")
 public interface ServiceBClient {
